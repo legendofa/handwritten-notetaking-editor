c153abac7ca2bad19e2b5eb7f02dfa92f57be764
st170001 <st170001@stud.uni-stuttgart.de>
2021-01-13 15:36:50 +0100

    Datatype refactoring ideas

---
+++ src/datatypes.rs
@@ -0,0 +1,12 @@
+use gio::prelude::*;
+use glib::*;
+use gtk::prelude::*;
+use gtk::*;
+
+pub struct Tool {
+	button: Button,
+}
+
+impl Tool {}
+
+pub struct Page {}
--- src/main.rs
+++ src/main.rs
@@ -5,28 +5,16 @@ use gtk::*;
 
 use std::env::args;
 
-fn build_ui(application: &gtk::Application) {
+pub fn build_ui(application: &Application) {
 	let window = gtk::ApplicationWindow::new(application);
 
 	window.set_title("Handwritten notetaking editor");
 	window.set_border_width(10);
-	window.set_position(gtk::WindowPosition::Center);
+	window.set_position(WindowPosition::Center);
 	window.set_default_size(800, 600);
 
 	let menu_bar = menu_bar(&window);
-
-	let button_0 = Button::with_label("Sidebar 0!");
-
-	button_0.connect_clicked(|_| {
-		println!("Hello");
-	});
-
-	let button_1 = Button::with_label("Sidebar 1!");
 	let button_2 = DrawingArea::new();
-	let button_3 = Button::with_label("Taskbutton 0!");
-	let button_4 = Button::with_label("Taskbutton 1!");
-	let button_5 = Button::with_label("Taskbutton 2!");
-	let button_6 = Button::with_label("Taskbutton 3!");
 
 	let vertical_pack_0 = Box::new(Orientation::Vertical, 0);
 	let vertical_pack_1 = Box::new(Orientation::Vertical, 0);
@@ -38,19 +26,46 @@ fn build_ui(application: &gtk::Application) {
 	vertical_pack_0.pack_start(&horizontal_pack_0, false, false, 0);
 	vertical_pack_0.pack_start(&horizontal_pack_1, true, true, 0);
 
-	horizontal_pack_0.pack_start(&button_3, false, false, 0);
-	horizontal_pack_0.pack_start(&button_4, false, false, 0);
-	horizontal_pack_0.pack_start(&button_5, false, false, 0);
-	horizontal_pack_0.pack_start(&button_6, false, false, 0);
+	for tool in tool_items() {
+		horizontal_pack_0.pack_start(&tool, false, false, 0);
+		tool.connect_clicked(|_| {
+			println!("Use this tool...");
+		});
+	}
 
 	horizontal_pack_1.pack_start(&vertical_pack_1, false, false, 0);
 	horizontal_pack_1.pack_start(&button_2, true, true, 0);
 
-	vertical_pack_1.pack_start(&button_0, false, false, 0);
-	vertical_pack_1.pack_start(&button_1, false, false, 0);
+	for page in pages() {
+		vertical_pack_1.pack_start(&page, false, false, 0);
+		page.connect_clicked(|_| {
+			println!("Go to this page...");
+		});
+	}
+
+	drawing_mechanics(button_2);
 
 	window.add(&vertical_pack_0);
 	window.show_all();
+}
+
+fn pages() -> Vec<Button> {
+	vec![
+		Button::with_label("Page 1"),
+		Button::with_label("Page 2"),
+		Button::with_label("Page 3"),
+		Button::with_label("Page 4"),
+		Button::with_label("Page 5"),
+	]
+}
+
+fn tool_items() -> Vec<Button> {
+	vec![
+		Button::with_label("Undo"),
+		Button::with_label("Redo"),
+		Button::with_label("Paintbrush"),
+		Button::with_label("Eraser"),
+	]
 }
 
 fn menu_bar(window: &ApplicationWindow) -> MenuBar {
@@ -77,6 +92,59 @@ fn menu_bar(window: &ApplicationWindow) -> MenuBar {
 	menu_bar.append(&file);
 
 	menu_bar
+}
+
+fn drawing_mechanics(area: DrawingArea) {
+	area.connect_draw(|_, cr| {
+		cr.set_dash(&[3., 2., 1.], 1.);
+
+		cr.scale(500f64, 500f64);
+
+		cr.set_source_rgb(250.0 / 255.0, 224.0 / 255.0, 55.0 / 255.0);
+		cr.paint();
+
+		cr.set_line_width(0.05);
+
+		// border
+		cr.set_source_rgb(0.3, 0.3, 0.3);
+		cr.rectangle(0.0, 0.0, 1.0, 1.0);
+		cr.stroke();
+
+		cr.set_line_width(0.03);
+
+		// draw circle
+		cr.arc(0.5, 0.5, 0.4, 0.0, 3.14 * 2.);
+		cr.stroke();
+
+		// mouth
+		let mouth_top = 0.68;
+		let mouth_width = 0.38;
+
+		let mouth_dx = 0.10;
+		let mouth_dy = 0.10;
+
+		cr.move_to(0.50 - mouth_width / 2.0, mouth_top);
+		cr.curve_to(
+			0.50 - mouth_dx,
+			mouth_top + mouth_dy,
+			0.50 + mouth_dx,
+			mouth_top + mouth_dy,
+			0.50 + mouth_width / 2.0,
+			mouth_top,
+		);
+
+		cr.stroke();
+
+		let eye_y = 0.38;
+		let eye_dx = 0.15;
+		cr.arc(0.5 - eye_dx, eye_y, 0.05, 0.0, 3.14 * 2.);
+		cr.fill();
+
+		cr.arc(0.5 + eye_dx, eye_y, 0.05, 0.0, 3.14 * 2.);
+		cr.fill();
+
+		Inhibit(false)
+	});
 }
 
 fn main() {
