2c3d56d87690ab4260503cfd392372ff1c2732bf
st170001 <st170001@stud.uni-stuttgart.de>
2021-01-13 12:56:38 +0100

    Improved layout

--- src/main.rs
+++ src/main.rs
@@ -15,19 +15,24 @@ fn build_ui(application: &gtk::Application) {
 
 	let menu_bar = menu_bar();
 
-	let button_0 = gtk::Button::with_label("Sidebar 0!");
-	let button_1 = gtk::Button::with_label("Sidebar 1!");
-	let button_2 = gtk::Button::with_label("Draw here!");
-	let button_3 = gtk::Button::with_label("Taskbutton 0!");
-	let button_4 = gtk::Button::with_label("Taskbutton 1!");
-	let button_5 = gtk::Button::with_label("Taskbutton 2!");
-	let button_6 = gtk::Button::with_label("Taskbutton 3!");
+	let button_0 = Button::with_label("Sidebar 0!");
 
-	let vertical_pack_0 = gtk::Box::new(Orientation::Vertical, 0);
-	let vertical_pack_1 = gtk::Box::new(Orientation::Vertical, 0);
+	button_0.connect_clicked(|_| {
+		println!("Hello");
+	});
 
-	let horizontal_pack_0 = gtk::Box::new(Orientation::Horizontal, 0);
-	let horizontal_pack_1 = gtk::Box::new(Orientation::Horizontal, 0);
+	let button_1 = Button::with_label("Sidebar 1!");
+	let button_2 = Button::with_label("Draw here!");
+	let button_3 = Button::with_label("Taskbutton 0!");
+	let button_4 = Button::with_label("Taskbutton 1!");
+	let button_5 = Button::with_label("Taskbutton 2!");
+	let button_6 = Button::with_label("Taskbutton 3!");
+
+	let vertical_pack_0 = Box::new(Orientation::Vertical, 0);
+	let vertical_pack_1 = Box::new(Orientation::Vertical, 0);
+
+	let horizontal_pack_0 = Box::new(Orientation::Horizontal, 0);
+	let horizontal_pack_1 = Box::new(Orientation::Horizontal, 0);
 
 	vertical_pack_0.pack_start(&menu_bar, false, false, 0);
 	vertical_pack_0.pack_start(&horizontal_pack_0, false, false, 0);
@@ -52,41 +57,24 @@ fn menu_bar() -> MenuBar {
 	let menu = Menu::new();
 	let menu_bar = MenuBar::new();
 	let file = MenuItem::with_label("File");
-	let file_item = MenuItem::new();
-	let file_box = gtk::Box::new(gtk::Orientation::Horizontal, 0);
-	let file_image = Image::from_file("resources/file.png");
-	let file_label = Label::new(Some("File"));
-	let folder_item = MenuItem::new();
-	let folder_box = gtk::Box::new(gtk::Orientation::Horizontal, 0);
-	let folder_image = Image::from_icon_name(Some("folder-music-symbolic"), IconSize::Menu);
-	let folder_label = Label::new(Some("Folder"));
+	let open_file = MenuItem::new();
+	open_file.add(&Label::new(Some("Open File")));
 
-	file_box.pack_start(&file_image, false, false, 0);
-	file_box.pack_start(&file_label, true, true, 0);
-	file_item.add(&file_box);
-	folder_box.pack_start(&folder_image, false, false, 0);
-	folder_box.pack_start(&folder_label, true, true, 0);
-	folder_item.add(&folder_box);
-	menu.append(&file_item);
-	menu.append(&folder_item);
+	open_file.connect_activate(|_| {
+		println!("File should be openend here...");
+	});
+
+	let save_file = MenuItem::new();
+	save_file.add(&Label::new(Some("Save File")));
+
+	save_file.connect_activate(|_| {
+		println!("File should be saved here...");
+	});
+
+	menu.append(&open_file);
+	menu.append(&save_file);
 	file.set_submenu(Some(&menu));
 	menu_bar.append(&file);
-
-	let other_menu = Menu::new();
-	let sub_other_menu = Menu::new();
-	let other = MenuItem::with_label("About");
-	let sub_other = MenuItem::with_label("This is");
-	let sub_other2 = MenuItem::with_label("a prototype");
-	let sub_sub_other2 = MenuItem::with_label("Thats");
-	let sub_sub_other2_2 = MenuItem::with_label("right");
-
-	sub_other_menu.append(&sub_sub_other2);
-	sub_other_menu.append(&sub_sub_other2_2);
-	sub_other2.set_submenu(Some(&sub_other_menu));
-	other_menu.append(&sub_other);
-	other_menu.append(&sub_other2);
-	other.set_submenu(Some(&other_menu));
-	menu_bar.append(&other);
 
 	menu_bar
 }
